7c01a63a34487911c437b38b6ebc8d958a801dcd
Ife Odugbesan <ifeodugbesan@yahoo.com>
2024-06-21 22:10:22 +0100

    Implemented Landing page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  get "home", to: "pages#home";
+  root "pages#home"
 end
